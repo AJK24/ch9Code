6b03ce9ad17bb50d8d2918eaadcc02d3d2e27b79
Andrew <akv9509@saintjoe.edu>
2015-02-21 18:14:58 -0500

    Added the code from the book

---
+++ 9.3.js
@@ -0,0 +1,41 @@
+//Andrew Klump
+//example 9.3
+
+
+
+load("set.js");
+var cis = new Set();
+cis.add("Mike");
+cis.add("Clayton");
+cis.add("Jennifer");
+cis.add("Raymond");
+var dmp = new Set();
+dmp.add("Raymond");
+dmp.add("Cynthia");
+dmp.add("Bryan");
+var inter = cis.intersect(dmp);
+print(inter.show()); // displays Raymond
+
+
+function subset(set) {
+	if (this.size() > set.size()) {
+		return false;
+	}
+	else {
+		for each (var member in this.dataStore) {
+			if (!set.contains(member)) {
+				return false;
+			}
+		}
+	}
+	return true;
+}
+
+
+
+
+function size() {
+	return this.dataStore.length;
+}
+
+
---
+++ 9.5.js
@@ -0,0 +1,19 @@
+//Andrew Klump
+//Example 9.5
+
+
+
+load("set.js");
+var cis = new Set();
+var it = new Set();
+cis.add("Clayton");
+cis.add("Jennifer");
+cis.add("Danny");
+it.add("Bryan");
+it.add("Clayton");
+it.add("Jennifer");
+var diff = new Set();
+diff = cis.difference(it);
+print("[" + cis.show() + "] difference [" + it.show()
+		+ "] -> [" + diff.show() + "]");
+
